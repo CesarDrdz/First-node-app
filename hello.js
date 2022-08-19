a78a2fbad9e4528ccb1718aa14a83b1eedf5b940
John Doe <johndoe@example.com>
2022-08-19 00:56:59 -0600

    used the process.argv to run a small app

---
+++ hello.js
@@ -0,0 +1,10 @@
+const mission = process.argv[2];
+
+if(mission === 'learn') {
+    console.log('Time to writre some Node Code!!');
+} else {
+    console.log(`Is ${mission} really more fun?`);
+}
+
+// used the process.argv to run a small app 
+
